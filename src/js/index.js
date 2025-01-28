2b87842edbcd30b753104ce1caec27a5b548a01e
Kirill <kipabelousov@edu.hse.ru>
2025-01-28 23:14:08 +0300

    My work almost completed

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
  let quetionnItem = document.querySelectorAll('.quetions__item');
 
-        quetionnItem.forEach((item, index) => {
-            const number = (index + 1).toString().padStart(2, '0');
-            item.style.setProperty('--item-number', '"' + number + '"');
-        });
+    quetionnItem.forEach((item, index) => {
+        const number = (index + 1).toString().padStart(2, '0');
+        item.style.setProperty('--item-number', '"' + number + '"');
+    });
 const selectSingle = document.querySelector('.__select');
 const selectSingle_title = selectSingle.querySelector('.__select__title');
 const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
@@ -76,7 +76,45 @@ let swiper = new Swiper (".swiper", {
     
   }
 })
-// const swiper_1 = document.querySelector('.swiper').swiper;
+let swiper_2 = new Swiper (".swiper_2", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  speed: 400,
+  preventClicks: true,
+  noSwiping: true,
+  freeMode: false,
+  navigation:{
+    nextEl: ".next_2",
+    prevEl: ".prev_2",
+  },
+  breakpoints: {
+    550: {
+      slidesPerView: 2, 
+      spaceBetween: 20,
+    }, 
+    
+  }
+})
+const inputs = document.querySelectorAll('input'); // Получаем все input элементы
+const addedClasses = new WeakSet(); // Создаем WeakSet для отслеживания добавленных классов
 
-// // Now you can use all slider methods like
-// swiper.slideNext();
+inputs.forEach(input => {
+    input.addEventListener('focus', function() {
+        const nameDiv = this.nextElementSibling; // Находим следующий элемент, div.name
+
+        if (nameDiv && nameDiv.classList.contains('name')) { // Если это div.name
+          if (!addedClasses.has(nameDiv)){ // Проверяем, добавляли ли мы уже класс
+              nameDiv.classList.add('active-placeholder'); // Добавляем класс
+              addedClasses.add(nameDiv);  // Помечаем, что класс добавлен
+          }
+        }
+    });
+});
+import { animate } from "motion"
+const count = document.getElementById("count")
+
+  animate(0, 800, {
+      duration: 2,
+      ease: "circOut",
+      onUpdate: (latest) => (count.innerHTML = Math.round(latest)),
+  })
